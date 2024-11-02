5ae8abd8ad922c0b1403f12697e463d008806dd3
Rik Rosseel <contact@rikrosseel.com>
2024-11-02 20:34:54 +0100

    Add form input handling

--- front-end/components/speedrun-submitter.tsx
+++ front-end/components/speedrun-submitter.tsx
@@ -1,15 +1,20 @@
 import CategoryService from "@services/category-service";
 import GameService from "@services/game-service";
 import SpeedrunService from "@services/speedrun-service";
-import { Category, Game } from "@types";
+import { Category, Game, SpeedrunInput } from "@types";
 import { useEffect, useState } from "react";
 
 const SpeedrunSubmitter: React.FC = () => {
 
+    const [time, setTime] = useState<number>(Number.NaN);
+
+    const [videoLink, setVideoLink] = useState<string>("");
+
     const [games, setGames] = useState<Array<Game>>([]);
     const [selectedGame, setSelectedGame] = useState<Game | null>(null);
-
+    
     const [categories, setCategories] = useState<Array<Category>>([]);
+    const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
 
     const getGames = async () => {
         const [response] = await Promise.all([GameService.getAllGames()]);
@@ -24,20 +29,29 @@ const SpeedrunSubmitter: React.FC = () => {
     }
 
     useEffect(() => {
-        // getGames();
+        getGames();
     }, []);
 
     useEffect(() => {
         if (selectedGame) {
-            // getCategories(selectedGame);
+            getCategories(selectedGame);
         } else {
             setCategories([]);
         }
     }, [selectedGame]);
 
 
-    const handleSpeedrunFormSubmit = () => {
-        // SpeedrunService.postSpeedrun()
+    const handleSpeedrunFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        // TODO: get user id, not implemented yet (global or prop?)
+        const userId: number = 1;
+        
+        if (selectedGame  && selectedCategory) {
+            const speedrunInput: SpeedrunInput = {time, videoLink, gameId: selectedGame.id!, categoryId: selectedCategory.id!, userId};
+            const [response] = await Promise.all([SpeedrunService.postSpeedrun(speedrunInput)]);
+            const [json] = await Promise.all([response.json()]);
+        } else {
+            event.preventDefault()
+        }
     }
 
     return (
@@ -59,26 +73,32 @@ const SpeedrunSubmitter: React.FC = () => {
                             <form onSubmit={handleSpeedrunFormSubmit}>
                             <div className="mb-3">
                                 <label htmlFor="time" className="col-form-label">Speedrun Time:</label>
-                                <input type="time" className="form-control" step={0.001} required></input>
+                                <input type="time"className="form-control" id="time" step={0.001} onChange={(event) => {
+                                    const input = event.target as HTMLInputElement;
+                                    setTime(input.valueAsNumber)}} required></input>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="video-link" className="col-form-label">Video Link:</label>
-                                <input type="url" className="form-control" id="video-link" placeholder="https://www.example.com/..."required></input>
+                                <input type="url" className="form-control" id="video-link" placeholder="https://www.example.com/..." onChange={(event) => {
+                                    const input = event.target as HTMLInputElement;
+                                    setVideoLink(input.value);
+                                }} required></input>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="game" className="col-form-label">Game:</label>
-                                <select className="form-select" id="game" required>
-                                    <option selected onChange={() => setSelectedGame(null)}>Choose a game</option>
+                                <select className="form-select" id="game" defaultValue="" required>
+                                    <option value="" onChange={() => setSelectedGame(null)}>Choose a game</option>
                                     {games.map((game, index) => <option key={index} value={game.id} onChange={() => setSelectedGame(game)}>{game.name}</option>)}
                                 </select>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="category" className="col-form-label">Category:</label>
-                                <select className="form-select" id="category" disabled={!selectedGame} required>
-                                    {categories.map((category, index) => <option key={index} value={category.id}>{category.name}</option>)}
+                                <select className="form-select" id="category" disabled={!selectedGame} defaultValue="" required>
+                                    <option value="" onChange={() => setSelectedCategory(null)}>Choose a category</option>
+                                    {categories.map((category, index) => <option key={index} value={category.id} onChange={() => setSelectedCategory(category)}>{category.name}</option>)}
                                 </select>
                             </div>
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <input type="submit" className="btn btn-primary" value="Submit"/>
                             </form>
                         </div>
 
