78974e99cbf34043b73d619359e8181291756e79
Rik Rosseel <contact@rikrosseel.com>
2024-11-02 17:37:35 +0100

    Add types

---
+++ front-end/types/index.ts
@@ -0,0 +1,45 @@
+export type Game = {
+    id?: number;
+    name: string;
+    genre: string;
+    description: string;
+    releaseDate: Date;
+};
+
+export type Category = {
+    id?: number;
+    name: string;
+    description: string;
+    game: Game;
+};
+
+export type Speedrun = {
+    id?: number;
+    time: number;
+    submitDate: Date;
+    videoLink: string;
+    isValidated: boolean;
+    speedrunner: User;
+    validator?: User;
+    game: Game;
+    category: Category;
+};
+
+export type SpeedrunInput = {
+    userId: number;
+    gameId: number;
+    categoryId: number;
+    videoLink: string;
+    time: number;
+};
+
+export type User = {
+    id?: number;
+    username: string;
+    email: string;
+    password: string;
+    signUpDate: Date;
+    role: Role;
+};
+
+export type Role = 'User' | 'Admin';
