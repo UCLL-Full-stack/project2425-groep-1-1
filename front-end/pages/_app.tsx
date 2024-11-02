94f4b61aecaf05cc972a8214de2a7603d8f9eed2
Rik Rosseel <contact@rikrosseel.com>
2024-11-02 17:38:20 +0100

    Basic app setup

---
+++ front-end/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import "@styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+
+    useEffect(() => {
+        require('bootstrap/dist/js/bootstrap.bundle.min.js');
+    },[]);
+
+    return <Component {...pageProps} />;
+}
