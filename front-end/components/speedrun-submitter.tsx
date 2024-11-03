6b1e009014449c36b5032807a75eb19e45798ef3
rik-rosseel <rik.rosseel@proton.me>
2024-11-03 14:14:16 +0100

    Refactor speedrun submitter

--- front-end/components/speedrun-submitter.tsx
+++ front-end/components/speedrun-submitter.tsx
@@ -11,10 +11,10 @@ const SpeedrunSubmitter: React.FC = () => {
     const [videoLink, setVideoLink] = useState<string>("");
 
     const [games, setGames] = useState<Array<Game>>([]);
-    const [selectedGame, setSelectedGame] = useState<Game | null>(null);
-    
+    const [selectedGameId, setSelectedGameId] = useState<number>(Number.NaN);
+
     const [categories, setCategories] = useState<Array<Category>>([]);
-    const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
+    const [selectedCategoryId, setSelectedCategoryId] = useState<number>(Number.NaN);
 
     const getGames = async () => {
         const [response] = await Promise.all([GameService.getAllGames()]);
@@ -22,8 +22,8 @@ const SpeedrunSubmitter: React.FC = () => {
         setGames(json);
     }
 
-    const getCategories = async (game: Game) => {
-        const [response] = await Promise.all([CategoryService.getAllCategoriesForGame(game)]);
+    const getCategories = async ({ id }: { id: number }) => {
+        const [response] = await Promise.all([CategoryService.getAllCategoriesByGameId({ id })]);
         const [json] = await Promise.all([response.json()]);
         setCategories(json);
     }
@@ -33,25 +33,29 @@ const SpeedrunSubmitter: React.FC = () => {
     }, []);
 
     useEffect(() => {
-        if (selectedGame) {
-            getCategories(selectedGame);
+        if (!isNaN(selectedGameId)) {
+            getCategories({ id: selectedGameId });
         } else {
             setCategories([]);
         }
-    }, [selectedGame]);
+    }, [selectedGameId]);
 
+
+    const onGameInputChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedGameId(Number(event.target.value));
+    }
+    const onCategoryInputChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedCategoryId(Number(event.target.value));
+    }
 
     const handleSpeedrunFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
         // TODO: get user id, not implemented yet (global or prop?)
         const userId: number = 1;
-        
-        if (selectedGame  && selectedCategory) {
-            const speedrunInput: SpeedrunInput = {time, videoLink, gameId: selectedGame.id!, categoryId: selectedCategory.id!, userId};
-            const [response] = await Promise.all([SpeedrunService.postSpeedrun(speedrunInput)]);
-            const [json] = await Promise.all([response.json()]);
-        } else {
-            event.preventDefault()
-        }
+
+        const speedrunInput: SpeedrunInput = { time, videoLink, gameId: selectedGameId, categoryId: selectedCategoryId, userId };
+        const [response] = await Promise.all([SpeedrunService.postSpeedrun(speedrunInput)]);
+        const [json] = await Promise.all([response.json()]);
     }
 
     return (
@@ -71,34 +75,35 @@ const SpeedrunSubmitter: React.FC = () => {
                         </div>
                         <div className="modal-body">
                             <form onSubmit={handleSpeedrunFormSubmit}>
-                            <div className="mb-3">
-                                <label htmlFor="time" className="col-form-label">Speedrun Time:</label>
-                                <input type="time"className="form-control" id="time" step={0.001} onChange={(event) => {
-                                    const input = event.target as HTMLInputElement;
-                                    setTime(input.valueAsNumber)}} required></input>
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="video-link" className="col-form-label">Video Link:</label>
-                                <input type="url" className="form-control" id="video-link" placeholder="https://www.example.com/..." onChange={(event) => {
-                                    const input = event.target as HTMLInputElement;
-                                    setVideoLink(input.value);
-                                }} required></input>
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="game" className="col-form-label">Game:</label>
-                                <select className="form-select" id="game" defaultValue="" required>
-                                    <option value="" onChange={() => setSelectedGame(null)}>Choose a game</option>
-                                    {games.map((game, index) => <option key={index} value={game.id} onChange={() => setSelectedGame(game)}>{game.name}</option>)}
-                                </select>
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="category" className="col-form-label">Category:</label>
-                                <select className="form-select" id="category" disabled={!selectedGame} defaultValue="" required>
-                                    <option value="" onChange={() => setSelectedCategory(null)}>Choose a category</option>
-                                    {categories.map((category, index) => <option key={index} value={category.id} onChange={() => setSelectedCategory(category)}>{category.name}</option>)}
-                                </select>
-                            </div>
-                            <input type="submit" className="btn btn-primary" value="Submit"/>
+                                <div className="mb-3">
+                                    <label htmlFor="time" className="col-form-label">Speedrun Time:</label>
+                                    <input type="time" className="form-control" id="time" step={0.001} onChange={(event) => {
+                                        const input = event.target as HTMLInputElement;
+                                        setTime(input.valueAsNumber)
+                                    }} required></input>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="video-link" className="col-form-label">Video Link:</label>
+                                    <input type="url" className="form-control" id="video-link" placeholder="https://www.example.com/..." onChange={(event) => {
+                                        const input = event.target as HTMLInputElement;
+                                        setVideoLink(input.value);
+                                    }} required></input>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="game" className="col-form-label">Game:</label>
+                                    <select className="form-select" id="game" defaultValue="" onChange={onGameInputChange} required>
+                                        <option value="" disabled>Choose a game</option>
+                                        {games.map((game, index) => <option key={game.id} value={game.id}>{game.name}</option>)}
+                                    </select>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="category" className="col-form-label">Category:</label>
+                                    <select className="form-select" id="category" defaultValue="" onChange={onCategoryInputChange} disabled={!selectedGameId} required>
+                                        <option value="" disabled>Choose a category</option>
+                                        {categories.map((category, index) => <option key={category.id} value={category.id}>{category.name}</option>)}
+                                    </select>
+                                </div>
+                                <input type="submit" className="btn btn-primary" value="Submit" />
                             </form>
                         </div>
 
