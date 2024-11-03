0d5e2e26a2a1d1be8c51352bc417099618fb03e8
Rik Rosseel <contact@rikrosseel.com>
2024-11-03 18:42:47 +0100

    Add service test for getAllGames

---
+++ back-end/test/service/game.service.test.ts
@@ -0,0 +1,33 @@
+import { Game } from '../../model/game';
+import gameDb from '../../repository/game.db';
+import gameService from '../../service/game.service';
+
+let mockGameDbGetAllGames: jest.Mock;
+
+const games: Array<Game> = [
+    new Game({
+        id: 1,
+        name: 'Super Mario',
+        description: 'Some game description',
+        genre: 'Platformer',
+        releaseDate: '1985-09-13',
+    }),
+];
+
+beforeEach(() => {
+    mockGameDbGetAllGames = jest.fn();
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+test('given ..., when getting all games, then all games are returned', () => {
+    // given
+    gameDb.getAllGames = mockGameDbGetAllGames.mockReturnValue(games);
+    // when
+    const result = gameService.getAllGames();
+    // then
+    expect(mockGameDbGetAllGames).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(games);
+});
