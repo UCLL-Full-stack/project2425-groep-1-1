2b8a3a8fa910cb1046eaca3f3ce6092afada52fa
marnix-willaert <marnix.willaert@student.ucll.be>
2024-12-12 17:04:07 +0100

    Added UserTable to homepage

--- front-end/components/users/UserTable.tsx
+++ front-end/components/users/UserTable.tsx
@@ -1,29 +1,32 @@
-
 const UserTable: React.FC = () => {
-  const users = [
-    { username: 'user1', password: 'user1', role: 'user' },
-    { username: 'user2', password: 'user2', role: 'admin' },
-    { username: 'user3', password: 'user3', role: 'organizer' },
-  ];
+    const users = [
+        { username: 'user1', password: 'user1', role: 'user' },
+        { username: 'user2', password: 'user2', role: 'admin' },
+        { username: 'user3', password: 'user3', role: 'organizer' },
+    ];
 
-  return (
-    <table>
-      <thead>
-      <th>username</th>
-      <th>password</th>
-      <th>role</th>
-      </thead>
-      <tbody>
-      {users.map((user, index) => (
-        <tr key={index}>
-          <td>{user.username}</td>
-          <td>{user.password}</td>
-          <td>{user.role}</td>
-        </tr>
-      ))}
-      </tbody>
-    </table>
-  )
-}
+    return (
+        <div className="d-flex justify-content-center">
+            <table className="table-sm table-bordered">
+                <thead>
+                    <tr>
+                        <th>username</th>
+                        <th>password</th>
+                        <th>role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user, index) => (
+                        <tr key={index}>
+                            <td>{user.username}</td>
+                            <td>{user.password}</td>
+                            <td>{user.role}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
 
 export default UserTable;
