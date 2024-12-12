2fed6da20baa60738eb6501ac7f325bfc7a9feb9
Rik Rosseel <contact@rikrosseel.com>
2024-12-12 20:49:35 +0100

    Rename DTO

--- back-end/service/user.service.ts
+++ back-end/service/user.service.ts
@@ -1,6 +1,6 @@
 import { User } from '../model/user';
 import userDb from '../repository/user.db';
-import { AuthenticationResponse, UserInput } from '../types';
+import {AuthenticationRequest, AuthenticationResponse, UserInput} from '../types';
 import bcrypt from "bcrypt";
 import { generateJwtToken } from '../util/jwt';
 
@@ -23,7 +23,7 @@ const createUser = async (userInput: UserInput): Promise<User> => {
   return await userDb.addUser(newUser);
 }
 
-const authenticate = async ({ username, password }: UserInput): Promise<AuthenticationResponse> => {
+const authenticate = async ({ username, password }: AuthenticationRequest): Promise<AuthenticationResponse> => {
   const user = await userDb.getUserByUsername({ username });
   if (!user) {
     throw new Error(`User with username: ${username} does not exist.`);
