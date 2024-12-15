89ce5d0e985f3db604708c99fe3ecc336dabf6ae
marnix-willaert <marnix.willaert@student.ucll.be>
2024-12-15 21:40:17 +0100

    Added GetGameById and GetCategoryByGameId functions to GameService

--- front-end/services/GameService.ts
+++ front-end/services/GameService.ts
@@ -8,8 +8,30 @@ const getAllGames = async () => {
     });
 };
 
+const getCategoryByGameId = async ({ id }: { id: string }) => {
+    return await fetch(process.env.NEXT_PUBLIC_API_URL + `/categories/game/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('loggedInUser') as string)?.token,
+        },
+    });
+}
+
+const getGameById = async ({ id }: { id: string }) => {
+    return await fetch(process.env.NEXT_PUBLIC_API_URL + `/games/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('loggedInUser') as string)?.token,
+        },
+    });
+}
+
 const GameService = {
     getAllGames,
+    getCategoryByGameId,
+    getGameById
 };
 
 export default GameService;
