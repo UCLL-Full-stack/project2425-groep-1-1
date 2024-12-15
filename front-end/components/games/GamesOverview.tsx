f87682b1251e3ac5b8cb1a8372d8a0f1a26fd6af
Rik Rosseel <contact@rikrosseel.com>
2024-12-15 16:04:58 +0100

    Add translation and link to game overview page

--- front-end/components/games/GamesOverview.tsx
+++ front-end/components/games/GamesOverview.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {Game} from "@types";
+import Link from "next/link";
+import {useTranslation} from "next-i18next";
 
 type Props = {
   games: Game[];
@@ -7,24 +9,24 @@ type Props = {
 
 const GamesOverview: React.FC<Props> = ({ games }: Props) => {
 
-  console.log(games);
+  const { t } = useTranslation();
 
   return (
     <>
-      <div className="container text-center mt-6">
-        <div className="row row-cols-4">
+      <div className="container-md mt-6">
+        <div className="row row-cols-lg-4 row-cols-md-3 row-cols-sm-1 g-4">
           {games.map((game: Game) => (
             <div className="col" key={game.id}>
-              <div className="card">
+              <div className="card h-100">
                 <div className="card-body">
                   <h4 className="card-title">{game.name}</h4>
+                  <p className="card-text">{game.description}</p>
+                  <Link href={"/games/" + game.id} className="btn btn-secondary stretched-link">{t("games.game.overview-button")}</Link>
                 </div>
               </div>
             </div>
           ))}
         </div>
-
-
       </div>
     </>
   );
