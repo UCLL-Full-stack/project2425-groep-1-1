2485bab6be3a80b398b585c811c3f486ddd2593e
marnix-willaert <marnix.willaert@student.ucll.be>
2024-12-18 20:43:44 +0100

    Added addUserToSpeedrunEvent to speedruneventservice

--- front-end/services/SpeedrunEventService.ts
+++ front-end/services/SpeedrunEventService.ts
@@ -1,4 +1,3 @@
-
 const getAllSpeedrunEvents = async () => {
     return await fetch(process.env.NEXT_PUBLIC_API_URL + '/speedrun-events', {
         method: 'GET',
@@ -9,8 +8,20 @@ const getAllSpeedrunEvents = async () => {
     });
 }
 
+const addUserToSpeedrunEvent = async (userId: number, eventId: number) => {
+    return await fetch(process.env.NEXT_PUBLIC_API_URL + '/speedrun-events/add-user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('loggedInUser')!)?.token,
+        },
+        body: JSON.stringify({ userId, eventId }),
+    });
+}
+
 const SpeedrunEventService = {
     getAllSpeedrunEvents,
+    addUserToSpeedrunEvent,
 };
 
 export default SpeedrunEventService;
