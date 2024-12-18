238be80273aff491c69def150e5f26d931b88664
Rik Rosseel <contact@rikrosseel.com>
2024-12-18 16:24:18 +0100

    Add tests for creating and adding participants to speedrun event

--- back-end/service/speedrun_event.service.ts
+++ back-end/service/speedrun_event.service.ts
@@ -1,7 +1,17 @@
 import speedrunEventDb from '../repository/speedrun_event.db';
+import {SpeedrunEventAddParticipantsInput, SpeedrunEventInput} from "../types";
+import { SpeedrunEvent } from "../model/speedrun_event";
 
 const getAllSpeedrunEvents = async () => {
   return speedrunEventDb.getAllSpeedrunEvents();
 }
 
-export default { getAllSpeedrunEvents };
+const createSpeedrunEvent = async ({ name, startDate, endDate }: SpeedrunEventInput) => {
+  throw new Error("Not implemented.")
+}
+
+const addParticipantToSpeedrunEvent = async ({ speedrunEventInput, userInputs }: SpeedrunEventAddParticipantsInput): Promise<SpeedrunEvent> => {
+  throw new Error("Not implemented.");
+}
+
+export default { getAllSpeedrunEvents, createSpeedrunEvent, addParticipantToSpeedrunEvent };
---
+++ back-end/test/service/speedrun_event.service.test.ts
@@ -0,0 +1,98 @@
+import { User } from "../../model/user";
+import { SpeedrunEvent } from "../../model/speedrun_event";
+import { set } from "date-fns";
+import speedrunEventService from "../../service/speedrun_event.service";
+import speedrunEventDb from "../../repository/speedrun_event.db";
+import userDb from "../../repository/user.db";
+import { SpeedrunEventAddParticipantsInput, SpeedrunEventInput, UserInput } from "../../types";
+
+let mockSpeedrunEventDbCreateSpeedrunEvent: jest.Mock;
+let mockSpeedrunEventDbGetSpeedrunEventById: jest.Mock;
+let mockUserDbGetUserById: jest.Mock;
+let mockSpeedrunEventDbUpdateSpeedrunEventParticipants: jest.Mock;
+
+
+const userId: number = 1;
+const username = 'user1';
+const email = 'user1@example.com';
+const password = 'password';
+const user = new User({
+  id: userId,
+  username,
+  email,
+  password,
+  signUpDate: new Date('2022-10-10T00:00:00.000Z'),
+  role: 'User',
+});
+
+const speedrunEventId = 1;
+const name = "GDQ";
+const startDate = set(new Date(), { hours: 10 });
+const endDate = set(new Date(), { hours: 22 });
+const createdAt = new Date();
+const updatedAt = new Date();
+
+
+
+beforeEach(() => {
+
+  const speedrunEvent = new SpeedrunEvent({
+    id: speedrunEventId,
+    name,
+    startDate,
+    endDate,
+    participants: [],
+    createdAt,
+    updatedAt,
+  });
+
+  mockSpeedrunEventDbCreateSpeedrunEvent = jest.fn().mockImplementation((speedrunEvent: SpeedrunEvent) => speedrunEvent);
+
+  mockSpeedrunEventDbGetSpeedrunEventById = jest.fn().mockResolvedValue(speedrunEvent);
+  mockSpeedrunEventDbUpdateSpeedrunEventParticipants = jest.fn().mockImplementation((speedrunEvent: SpeedrunEvent) => speedrunEvent);
+  mockUserDbGetUserById = jest.fn().mockResolvedValue(user);
+})
+
+afterEach(() => {
+  jest.clearAllMocks();
+})
+
+test(`given: a valid speedrun event input, when: creating a speedrun event, then: a speedrun event is created`, async () => {
+  // given
+  speedrunEventDb.addSpeedrunEvent = mockSpeedrunEventDbCreateSpeedrunEvent
+  const speedrunEventInput: SpeedrunEventInput = { id: speedrunEventId, name, startDate, endDate};
+
+  // when
+  await speedrunEventService.createSpeedrunEvent(speedrunEventInput);
+
+  // then
+  expect(mockSpeedrunEventDbCreateSpeedrunEvent).toHaveBeenCalledTimes(1);
+  expect(mockSpeedrunEventDbCreateSpeedrunEvent).toHaveBeenCalledWith(new SpeedrunEvent({ name, startDate, endDate, participants: [] }));
+});
+
+test(`given: a speedrun event add participants input, when: adding a valid user as participant, then: the user is added as a participant`, async () => {
+  // given
+  const speedrunEventInput: SpeedrunEventInput = { id: speedrunEventId, name, startDate, endDate};
+  const userInput: UserInput = {
+    id: userId,
+    username,
+    email,
+    password,
+  }
+  const speedRunEventAddParticipantsInput: SpeedrunEventAddParticipantsInput = {
+    userInputs: [userInput],
+    speedrunEventInput,
+  }
+
+  speedrunEventDb.getSpeedrunEventById = mockSpeedrunEventDbGetSpeedrunEventById;
+  speedrunEventDb.updateSpeedrunEventParticipants = mockSpeedrunEventDbUpdateSpeedrunEventParticipants;
+  userDb.getUserById = mockUserDbGetUserById;
+
+  // when
+  const result = await speedrunEventService.addParticipantToSpeedrunEvent(speedRunEventAddParticipantsInput);
+
+  // then
+  expect(result.getParticipants()).toContain(user);
+});
+
+
