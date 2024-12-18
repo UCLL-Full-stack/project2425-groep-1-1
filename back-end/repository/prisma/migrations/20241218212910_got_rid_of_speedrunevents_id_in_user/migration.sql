2b07186523008dd853f3c934cb58b774d70e8052
marnix-willaert <marnix.willaert@student.ucll.be>
2024-12-18 22:29:46 +0100

    got rid of speedruneventsId in User

---
+++ back-end/repository/prisma/migrations/20241218212910_got_rid_of_speedrunevents_id_in_user/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `speedrunEventId` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "speedrunEventId";
