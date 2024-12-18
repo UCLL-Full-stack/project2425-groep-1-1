c1d0d0a0507c6fcd5cb79c6f9eb05d20d0d91e18
rik-rosseel <rik.rosseel@proton.me>
2024-12-18 11:13:03 +0100

    Add get speedrun by id method

--- back-end/repository/speedrun_event.db.ts
+++ back-end/repository/speedrun_event.db.ts
@@ -2,18 +2,31 @@ import database from '../util/database';
 import { SpeedrunEvent } from "../model/speedrun_event";
 
 const getAllSpeedrunEvents = async (): Promise<SpeedrunEvent[]> => {
-    try{
+    try {
         const speedRunEventPrismas = await database.speedrunEvent.findMany({
             include: { participants: true }
         });
         return speedRunEventPrismas.map((speedRunEventPrisma) => SpeedrunEvent.from(speedRunEventPrisma))
-    }catch (error) {
+    } catch (error) {
         console.log(error);
         throw new Error("Database error, see console for more information.");
     }
 }
 
-const addSpeedRunEvent = async ({name, startDate, endDate, participants, createdAt, updatedAt}: SpeedrunEvent) => {
+const getSpeedrunEventById = async ({ id }: { id: number}): Promise<SpeedrunEvent | null> => {
+    try {
+        const speedrunEventPrisma = await database.speedrunEvent.findUnique({
+            where: { id },
+            include: { participants: true },
+        });
+        return speedrunEventPrisma ? SpeedrunEvent.from(speedrunEventPrisma) : null;
+    } catch (error) {
+        console.log(error);
+        throw new Error("Database error, see console for more information.");
+    }
+}
+
+const addSpeedrunEvent = async ({name, startDate, endDate, participants, createdAt, updatedAt}: SpeedrunEvent) => {
     try{
         const speedrunEventPrisma = await database.speedrunEvent.create({
             data: {
@@ -34,4 +47,22 @@ const addSpeedRunEvent = async ({name, startDate, endDate, participants, created
     }
 }
 
-export default { getAllSpeedrunEvents, addSpeedRunEvent };
+const updateSpeedrunEventParticipants = async (speedrunEvent: SpeedrunEvent) => {
+    try {
+        const speedrunEventPrisma = await database.speedrunEvent.update({
+            where: { id: speedrunEvent.id },
+            data: {
+                participants: {
+                    connect: speedrunEvent.participants.map((participant) => ({ id: participant.getId() }))
+                },
+            },
+            include: { participants: true },
+        });
+        return speedrunEventPrisma ? SpeedrunEvent.from(speedrunEventPrisma) : null;
+    } catch (error) {
+        console.log(error);
+        throw new Error("Database error, see console for more information.");
+    }
+}
+
+export default { getAllSpeedrunEvents, getSpeedrunEventById, addSpeedRunEvent: addSpeedrunEvent, updateSpeedrunEventParticipants};
